4374830fbfd985a4ea1d14e69d0f6c61d7d313b5
Christopher Glantschnig <christopher.glantschnig@gmail.com>
2015-11-17 18:41:18 +0100

    created Aufgabe3

--- src/university/Aufgabe3/tasks/Aufgabe3.java
+++ src/university/Aufgabe3/tasks/Aufgabe3.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 /*
     Aufgabe3) Eindimensionales Array
 
@@ -31,16 +33,48 @@
     Zusatzfragen:
     1. Warum kann man in 'printArray' for-each-Schleifen (also Schleifen der Form for(... : ...) ...) verwenden,
        in 'createArray' und 'halveArray' aber nicht?
+       in printArray haben wir nur lesenden Zugriff. In createArray und halveArray möchten wir den Wert ändern
     2. Warum ist es möglich, dass 'halveArray' kein Ergebnis zurückgibt, die Auswirkungen der Methode aber dennoch
        sichtbar werden?
+       weil der Parameter nur ein Pointer ist und somit das Object, das gleiche ist wie im aufrufenden Programm
     3. Woran könnte man feststellen, ob die Lösung richtig ist, obwohl jeder Testlauf andere Ergebnisse liefert?
+       Man kann Tests schreiben, wenn die immer durchlaufen, kann man davon ausgehen, dass alles passt
 */
 public class Aufgabe3 {
 
     // Hier sollten die benötigten Methoden stehen.
+    public static double[] createArray(int count) {
+        double tmp[] = new double[count];
+        for(int i = 0; i < count; i++) {
+            tmp[i] = (Math.random() * 100) - 0;
+        }
+        return tmp;
+    }
+
+    public static void printArray(double[] array, int classes) {
+        int classCounters[] = new int[classes];
+        int size = 100 / classes; // size of every single class
+        for(double item : array) {
+            classCounters[ (int)(item / size) ] += 1;
+        }
+        for (int item : classCounters) {
+            System.out.println(item);
+        }
+    }
+
+    public static void halveArray(double[] array) {
+        for (int i = 0; i < array.length; i++) {
+            array[i] /= 2;
+        }
+    }
 
     // Just for testing ...
     public static void main(String[] args) {
         // Den Rumpf dieser Methode können Sie beliebig verändern.
+        double[] test = createArray(10);
+        System.out.println(Arrays.toString(test));
+        printArray(test, 3);
+        halveArray(test);
+        System.out.println(Arrays.toString(test));
     }
 }
